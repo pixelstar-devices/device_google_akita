eb476dd8f40820c48e61bab4fd8b372274ed50bf
Libra420T <ryantf420@gmail.com>
2024-08-28 19:15:28 -0400

    DISABLE ARTIFACT PATH REQUIREMENTS

--- BoardConfigPixelStar.mk
+++ BoardConfigPixelStar.mk
@@ -5,4 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/akita/aam_compatibility_matrix.xml
